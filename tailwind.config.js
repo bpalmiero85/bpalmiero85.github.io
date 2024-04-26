92136c8d668b0bfc133f23fcfcedaa11a8d8c9e4
bpalmiero85 <bpalmiero85@gmail.com>
2024-04-25 20:06:37 -0400

    Add placeholders for portfolio page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   
-  content: ['index.html','./src/**/*.{js,jsx,ts,tsx,vue,html}'],
+  content: ['index.html','portfolio.html','contact.html','./src/**/*.{js,jsx,ts,tsx,vue,html}'],
   theme: {
     extend: {},
   },
